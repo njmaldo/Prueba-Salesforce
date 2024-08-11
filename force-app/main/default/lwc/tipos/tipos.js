3be1f66a6e80d8cad1993711a0e76863beea025e
Néstor J. Maldonado <99345125+njmaldo@users.noreply.github.com>
2024-08-11 17:29:14 -0300

    2do Commit

---
+++ force-app/main/default/lwc/tipos/tipos.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+   
+export default class Tipos extends LightningElement {
+    tipos = { 
+        name:"Otro Libro",
+        descripcion: "Libro de no ficción",
+        Imagen_URL__C:
+        "https://http2.mlstatic.com/D_NQ_NP_719366-MLA77707224667_072024-O.webp"
+        
+    };
+}
