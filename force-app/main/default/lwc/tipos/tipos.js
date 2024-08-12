74615fc4838c823c5acbcb339a2e1b12d7c44ae7
Néstor J. Maldonado <99345125+njmaldo@users.noreply.github.com>
2024-08-12 10:09:12 -0300

    query apex

--- force-app/main/default/lwc/tipos/tipos.js
+++ force-app/main/default/lwc/tipos/tipos.js
@@ -6,6 +6,5 @@ export default class Tipos extends LightningElement {
         descripcion: "Libro de no ficción",
         Imagen_URL__C:
         "https://http2.mlstatic.com/D_NQ_NP_719366-MLA77707224667_072024-O.webp"
-        
     };
 }
